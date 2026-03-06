c5c63524b76c92762f37ab5f97f52b34b84c233a
Codalorian <evantijerina@protonmail.com>
2026-03-06 08:58:54 -0800

    add ollama

--- generation/ollama/ollama-gen.py
+++ generation/ollama/ollama-gen.py
@@ -1,1 +1,12 @@
 #name to ollama-gen.py to avoid circular input
+from ollama import chat
+
+stream = chat(
+    model='falcon3:3b', #change so user can pick the model
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
+
